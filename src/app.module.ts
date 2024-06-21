a5d283ab5be70780bd8850c2de1474419193b0b4
zhangxinxin <1285367184@qq.com>
2024-06-21 15:51:16 +0800

    安装并配置@nestjs/typeorm typeorm mysql实现连接本地数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './user/user.module';
 import { HomeworkModule } from './homework/homework.module';
 import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envFilePath = `.env.${process.env.NODE_ENV || 'developement'}`;
 console.log(envFilePath);
@@ -18,6 +19,18 @@ console.log(envFilePath);
       envFilePath,
       // 使用dotenv加载.env环境文件中的所有环境变量, 当访问了其他两个环境文件中不存在的环境变量时,就会去.env环境文件中查找!
       load: [() => dotenv.config({ path: '.env' })],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'testdb',
+      entities: [],
+      // 同步本地的schema与数据库
+      synchronize: true,
+      logging: ['error'],
     }),
   ],
   controllers: [],
